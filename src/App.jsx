fa04ecae57bf30a07a1a0f93f64c10c3a6bb26a6
unknown <daniel-alejo94@hotmail.com>
2022-03-19 20:28:35 -0600

    formulario formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Inicio from "./pages/Inicio";
+import EditarCliente from "./pages/EditarCliente";
+import NuevoCliente from "./pages/NuevoCliente";
+/*
+ *en Layout.jsx vamos a usar el Outlet que va a mostrar todos los componentes que esten
+ *anidados a Layout es algo asi como {children} puedes poner
+ *header antes de <Outlet /> y footer despues de <Outlet /> en Layout.jsx
+ *le ponemos index para que asi aparezca ese componente cuando se visite la url principal
+ * en este caso es y "/clientes"
+ *-- si le agregamos un path sera una ruta anidada, mostrando el Layout y ese componente */
+
 function App() {
-  return <h1>Hola</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import Alerta from "./Alerta";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const Formulario = () => {
+  /*creamos un schema de yup,
+  cuando no tome el error que le pongamos en los parentesis, usaremos
+  .typeError para definir uno especifico */
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre: Yup.string()
+      .min(3, "El nombre es muy corto")
+      .max(20, "El nombre es muy largo")
+      .required("El nombre del cliente es obligatorio"),
+    empresa: Yup.string().required("El nombre de la empresa es obligatorio"),
+    email: Yup.string()
+      .email("Email no valido")
+      .required("El email es obligatorio"),
+    telefono: Yup.number()
+      .integer("El número no es valido.")
+      .positive("El número no es valido.")
+      .typeError("El número no es valido."),
+  });
+  const handleSubmit = (valoresFormulario) => {};
+
+  return (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
+      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
+        Agregar Cliente
+      </h1>
+      <Formik
+        initialValues={{
+          nombre: "",
+          empresa: "",
+          email: "",
+          telefono: "",
+          notas: "",
+        }}
+        onSubmit={(valoresFormulario) => {
+          /*values son los valoresFormulario de initialValues pueden llevar cualquier nombre */
+          handleSubmit(valoresFormulario);
+        }}
+        validationSchema={nuevoClienteSchema}
+      >
+        {/* Estamos destricturando errores ya que formik tiene mucha informacion,
+        touched sirve para cuendo el usuario entro y salio del input se mostrara la alerta */}
+        {({ errors, touched }) => (
+          <Form className="mt-10">
+            <div className="mb-4">
+              <label className="text-gray-800" htmlFor="nombre">
+                Nombre:
+              </label>
+              <Field
+                className="mt-2 block w-full p-3 bg-gray-200"
+                id="nombre"
+                type="text"
+                placeholder="Nombre del Cliente"
+                name="nombre"
+              />
+              {errors.nombre && touched.nombre ? (
+                <Alerta>{errors.nombre}</Alerta>
+              ) : null}
+            </div>
+            <div className="mb-4">
+              <label className="text-gray-800" htmlFor="empresa">
+                Empresa:
+              </label>
+              <Field
+                className="mt-2 block w-full p-3 bg-gray-200"
+                id="empresa"
+                type="text"
+                placeholder="Empresa del Cliente"
+                name="empresa"
+              />
+              {errors.empresa && touched.empresa ? (
+                <Alerta>{errors.empresa}</Alerta>
+              ) : null}
+            </div>
+            <div className="mb-4">
+              <label className="text-gray-800" htmlFor="email">
+                Email:
+              </label>
+              <Field
+                className="mt-2 block w-full p-3 bg-gray-200"
+                id="email"
+                type="email"
+                placeholder="Email del Cliente"
+                name="email"
+              />
+              {errors.email && touched.email ? (
+                <Alerta>{errors.email}</Alerta>
+              ) : null}
+            </div>
+            <div className="mb-4">
+              <label className="text-gray-800" htmlFor="telefono">
+                Teléfono:
+              </label>
+              <Field
+                className="mt-2 block w-full p-3 bg-gray-200"
+                id="telefono"
+                type="tel"
+                placeholder="Teléfono del Cliente"
+                name="telefono"
+              />
+              {errors.telefono && touched.telefono ? (
+                <Alerta>{errors.telefono}</Alerta>
+              ) : null}
+            </div>
+            <div className="mb-4">
+              <label className="text-gray-800" htmlFor="notas">
+                Notas:
+              </label>
+              <Field
+                as="textarea"
+                className="mt-2 block w-full p-3 bg-gray-200 resize-none h-40"
+                id="notas"
+                type="email"
+                placeholder="Notas del Cliente"
+                name="notas"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Agregar Cliente"
+              className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Formulario;
